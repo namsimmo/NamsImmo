35263a135ba60c0c16f52ebaea9b359588a57980
Yannick Sereckissy <yannick.sereckissy@gmail.com>
2013-09-28 13:01:41 -0500

    rename project directory

--- administrator/components/com_namsImmo/namsImmo.php
+++
@@ -1,4 +0,0 @@
-<?php
-defined('_JEXEC') or die('Restricted Access');
-/// echo some debug text
-echo 'Welcome to namImmo back Office!';
--- components/com_namsImmo/namsImmo.php
+++
@@ -1,4 +0,0 @@
-<?php
-defined('_JEXEC') or die('Restricted Access');
-/// echo a debug Text
-echo 'Welcome to NamsImmo front end';
